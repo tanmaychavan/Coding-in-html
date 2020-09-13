d19fce95e49801e2bc2d9d5c0a9fb2f4d214de84
tanmaychavan <70258785+tanmaychavan@users.noreply.github.com>
2020-09-13 08:58:50 +0530

    Add files via upload

---
+++ [25]NEW.py
@@ -0,0 +1,29 @@
+import time
+
+print("Enter first number: ",end="")
+f_num = eval(input())
+print("Enter second number ",end="")
+s_num = eval(input())
+
+print("-------------------------------------")
+print("Your Entries: ")
+print("First number: ",f_num)
+print("Second number: ",s_num)
+
+"""
+if f_num > s_num:
+    print("Greater number: ",f_num)
+
+if s_num > f_num:
+    print("Greater number: ",s_num)
+
+"""
+
+if f_num > s_num:
+    print("True, Greater number: ",f_num)
+else:
+    print("False, Greater number: ",s_num)
+
+
+time.sleep(2.4)
+input()
